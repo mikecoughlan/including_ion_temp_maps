60a81842650a5810a261ebe865f2012c5cee1ac6
mikecoughlan <mike.k.coughlan@gmail.com>
2024-08-30 18:50:24 -0400

    update for dbht predictions

--- exe_swmag_canadian.sh
+++ exe_swmag_canadian.sh
@@ -4,8 +4,8 @@
 # defining a list of the region numbers to loop through
 
 # Loop through regions and run non_twins_modeling_final_version for each one
-python3 swmag_model.py --target 'rsd' --cluster 'canadian_cluster' --region 'CAN-0'
-python3 swmag_model.py --target 'rsd' --cluster 'canadian_cluster' --region 'CAN-1'
-python3 swmag_model.py --target 'rsd' --cluster 'canadian_cluster' --region 'CAN-2'
+python3 swmag_model.py --target 'dbht_max' --cluster 'canadian_cluster' --region 'CAN-0'
+python3 swmag_model.py --target 'dbht_max' --cluster 'canadian_cluster' --region 'CAN-1'
+python3 swmag_model.py --target 'dbht_max' --cluster 'canadian_cluster' --region 'CAN-2'
 
 
